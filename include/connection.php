1e5acc6657bdb8304fb08b853e88a6c2b54c79c1
Bayu Kurniawan <creatifycms@gmail.com>
2019-11-25 16:59:44 +0800

    Testing Heroku Postgres

--- include/connection.php
+++ include/connection.php
@@ -4,12 +4,24 @@
       $pdo = new PDO('mysql:host=localhost;dbname=todolistmagang', "root", "");
    }
    elseif ($host == 'todo-list-magang.herokuapp.com') {
-      $clearMysqlUrl = parse_url(getenv("CLEARDB_DATABASE_URL"));
+      // ClearDB Mysql
+      // $clearMysqlUrl = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-      $server   = $clearMysqlUrl["host"];
-      $username = $clearMysqlUrl["user"];
-      $password = $clearMysqlUrl["pass"];
-      $db       = substr($clearMysqlUrl["path"], 1);
-      $pdo      = new PDO('mysql:host='.$server.';dbname='.$db, $username, $password);
+      // $server   = $clearMysqlUrl["host"];
+      // $username = $clearMysqlUrl["user"];
+      // $password = $clearMysqlUrl["pass"];
+      // $db       = substr($clearMysqlUrl["path"], 1);
+      // $pdo      = new PDO('mysql:host='.$server.';dbname='.$db, $username, $password);
+   
+      // Heroku PostGres
+      $db = parse_url(getenv("DATABASE_URL"));
+      $pdo = new PDO("pgsql:" . sprintf(
+         "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+         $db["host"],
+         $db["port"],
+         $db["user"],
+         $db["pass"],
+         ltrim($db["path"], "/")
+      ));
    }
 ?>
---
+++ postgres.sql
@@ -0,0 +1,118 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.10
+-- Dumped by pg_dump version 9.6.10
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: _categories; Type: TABLE; Schema: public; Owner: rebasedata
+--
+
+CREATE TABLE public._categories (
+    id smallint,
+    name character varying(12) DEFAULT NULL::character varying,
+    created character varying(19) DEFAULT NULL::character varying
+);
+
+
+ALTER TABLE public._categories OWNER TO rebasedata;
+
+--
+-- Name: _lists; Type: TABLE; Schema: public; Owner: rebasedata
+--
+
+CREATE TABLE public._lists (
+    id smallint,
+    todo character varying(16) DEFAULT NULL::character varying,
+    category_id smallint,
+    user_id smallint,
+    assign character varying(10) DEFAULT NULL::character varying,
+    status character varying(1) DEFAULT NULL::character varying
+);
+
+
+ALTER TABLE public._lists OWNER TO rebasedata;
+
+--
+-- Name: _users; Type: TABLE; Schema: public; Owner: rebasedata
+--
+
+CREATE TABLE public._users (
+    id smallint,
+    first_name character varying(4) DEFAULT NULL::character varying,
+    last_name character varying(9) DEFAULT NULL::character varying,
+    username character varying(7) DEFAULT NULL::character varying,
+    email character varying(21) DEFAULT NULL::character varying,
+    password character varying(60) DEFAULT NULL::character varying,
+    created character varying(19) DEFAULT NULL::character varying
+);
+
+
+ALTER TABLE public._users OWNER TO rebasedata;
+
+--
+-- Data for Name: _categories; Type: TABLE DATA; Schema: public; Owner: rebasedata
+--
+
+COPY public._categories (id, name, created) FROM stdin;
+1	Belanjaan	2019-10-31 15:02:43
+2	Alat Sekolah	2019-10-31 15:08:36
+\.
+
+
+--
+-- Data for Name: _lists; Type: TABLE DATA; Schema: public; Owner: rebasedata
+--
+
+COPY public._lists (id, todo, category_id, user_id, assign, status) FROM stdin;
+1	Ayam 1kg	1	6	2019-10-31	
+2	Sayur ijo seiket	1	6	2019-10-31	1
+4	Daging Sapi	1	6	2019-11-04	1
+5	Pensil	2	6	2019-10-31	
+6	Penghapus	2	6	2019-10-31	
+7	Bumbu masak	1	6	2019-10-31	
+\.
+
+
+--
+-- Data for Name: _users; Type: TABLE DATA; Schema: public; Owner: rebasedata
+--
+
+COPY public._users (id, first_name, last_name, username, email, password, created) FROM stdin;
+6	Bayu	Kurniawan	baycore	creatifycms@gmail.com	$2y$10$PYS4Oyh/ygIqHPxjzkSxmO5lMFsdVXatQm3VMouOvij3cqFUZ6aia	2019-10-31 13:15:15
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
